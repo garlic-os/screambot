d24514f8c2f51ace91466953df75df97bee9375c
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-04-03 16:49:06 -0500

    Extract definitions to cpp file

--- rng.hpp
+++ rng.hpp
@@ -1,23 +1,11 @@
 #pragma once
 #include <cstdint>
-#include <random>
 #include <vector>
 
 
 namespace rng {
-	int64_t choose_number(int64_t min, int64_t max) {
-		static std::random_device rd;
-		static std::mt19937 gen(rd());
-		std::uniform_int_distribution<> dis(min, max);
-		return dis(gen);
-	}
-
+	int64_t choose_number(int64_t min, int64_t max);
 	template <typename T>
-	T choose_element(const std::vector<T>& vec) {
-		return vec[choose_number(0, vec.size() - 1)];
-	}
-
-	bool chance(double percent) {
-		return choose_number(0, 100) < percent;
-	}
+	T choose_element(const std::vector<T>& vec);
+	bool chance(double percent);
 }
