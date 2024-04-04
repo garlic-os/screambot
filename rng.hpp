ac1a2077c06e5ce20d472258308dd95066c0c927
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-04-03 21:57:46 -0500

    Revert "Extract definitions to cpp file"
    :husk: https://stackoverflow.com/questions/10632251/undefined-reference-to-template-function

--- rng.hpp
+++ rng.hpp
@@ -1,10 +1,22 @@
 #pragma once
 #include <cstdint>
+#include <random>
 #include <vector>
 
 namespace rng {
-	int64_t choose_number(int64_t min, int64_t max);
+	int64_t choose_number(int64_t min, int64_t max) {
+		static std::random_device rd;
+		static std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(min, max);
+		return dis(gen);
+	}
+
 	template <typename T>
-	T choose_element(const std::vector<T> &vec);
-	bool chance(double percent);
-}  // namespace rng
+	T choose_element(const std::vector<T>& vec) {
+		return vec[choose_number(0, vec.size() - 1)];
+	}
+
+	bool chance(double percent) {
+		return choose_number(0, 100) < percent;
+	}
+}
