0d7151d988c94a25ca412fee833634617f562b2d
Nate Kean <natekean65535@gmail.com>
2025-12-12 12:41:06 -0600

    ooer status

--- screambot.cpp
+++ screambot.cpp
@@ -73,7 +73,7 @@ Screambot::Screambot(const Config *config) {
 		m_client->set_presence(dpp::presence(
 			dpp::presence_status::ps_online,
 			dpp::activity_type::at_game,
-			"BAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+			"OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOER"
 		));
 	});
 
